4e2aeb3df85ebd4661823c750d3742ca00388d6b
AngeloZorzi <astro.drg666@gmail.com>
2025-06-03 14:05:28 +0200

    Initial commit

---
+++ src/main/java/it/S5D1/S5D1/S5D1Application.java
@@ -0,0 +1,13 @@
+package it.S5D1.S5D1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5D1Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/S5D1/S5D1/S5D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.S5D1.S5D1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
